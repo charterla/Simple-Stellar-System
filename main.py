f4f1e034becb304d2005acf8089acb741715365b
charterla <2330911429eric2@gmail.com>
2021-03-21 13:58:25 +0800

    Alpha_2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter
 import math
 import json
-from PIL import ImageGrab
 
 window = tkinter.Tk()
 window.geometry('600x350')
@@ -12,9 +11,9 @@ showBoard.pack()
 
 x0, y0 = 300, 175
 ang = -75 / 180 * math.pi
-rad = [70]
-xRad = 2
-yRad = 3
+rad = [120]
+xRad = 1
+yRad = 2
 
 preSin = math.sin(ang)
 preCos = math.cos(ang)
@@ -29,15 +28,6 @@ secondTemp = dXRad * (1 + preDCos) + dYRad * (1 - preDCos)
 thirdTemp = 2 * preSin * preCos * ((dYRad - dXRad) / dXRad / dYRad)
 fourthTemp = 2 * (preSin * preSin / dXRad + preCos * preCos / dYRad)
 
-
-def save():
-    global window, showBoard
-
-    x = window.winfo_rootx()+showBoard.winfo_x()
-    y = window.winfo_rooty()+showBoard.winfo_y()
-    ImageGrab.grab().crop((x, y, x+600, y+350)).save("saves.gif")
-
-
 showBoard.create_oval(x0-50, y0-50, x0+50, y0+50, fill="orange")
 for star in range(0, 1):
     nowX = sX
@@ -47,7 +37,7 @@ for star in range(0, 1):
     for i in range(10000, -10000, -1):
         lastX = json.loads(json.dumps(nowX))
         lastY = json.loads(json.dumps(nowY))
-        nowX = sX * i / 10000
+        nowX = sX * (i / 10000)
         nowY = (firstTemp * math.sqrt(secondTemp - 2 *
                                       nowX * nowX) + nowX * thirdTemp) / fourthTemp
 
@@ -57,7 +47,7 @@ for star in range(0, 1):
     for i in range(-10000, 10000, 1):
         lastX = json.loads(json.dumps(nowX))
         lastY = json.loads(json.dumps(nowY))
-        nowX = sX * i / 10000
+        nowX = sX * (i / 10000)
         nowY = (-1 * firstTemp * math.sqrt(secondTemp - 2 *
                                            nowX * nowX) + nowX * thirdTemp) / fourthTemp
 
@@ -67,8 +57,6 @@ for star in range(0, 1):
 
 def move(event):
     global showBoard, x0, y0, ang, rad, xRad, yRad, preSin, preCos, preDSin, preDCos, dXRad, dYRad, sX,  firstTemp, secondTemp, thirdTemp, fourthTemp
-    save()
-    img = tkinter.PhotoImage(file="saves.gif")
     way = -1
     i = 10000
 
@@ -76,7 +64,6 @@ def move(event):
     nowY = (firstTemp * math.sqrt(secondTemp - 2 *
                                   nowX * nowX) + nowX * thirdTemp) / fourthTemp
     while(True):
-        #showBoard.create_image(x0, y0, image=img)
         for star in range(0, 1):
             lastX = json.loads(json.dumps(nowX))
             lastY = json.loads(json.dumps(nowY))
@@ -92,20 +79,10 @@ def move(event):
             aX = json.loads(json.dumps(lastX))
             aY = json.loads(json.dumps(lastY))
 
-            #print(10 * abs(way * (40 * abs(nowY) + 5)))
-            while tTime <= 10 * abs(way * (40 * abs(nowY) + 5)) + 100:
-                tI += tWay
-                if i + tI > 10000 or i + tI < -10000:
-                    tWay *= -1
-                    tI += 2 * tWay
-                bX = json.loads(json.dumps(aX))
-                bY = json.loads(json.dumps(aY))
-                aX = sX * ((i + tI) / 10000)
-                aY = (tWay * firstTemp * math.sqrt(secondTemp -
-                                                   2 * aX * aX) + aX * thirdTemp) / fourthTemp
-                tTime += 1
-
-            while tTime <= 600:
+            track = 20 * abs(way * (40 * abs(nowY))) + 100
+            if track < 550:
+                track = 550
+            while tTime <= track:
                 tI += tWay
                 if i + tI > 10000 or i + tI < -10000:
                     tWay *= -1
@@ -127,7 +104,10 @@ def move(event):
 
             showBoard.update()
 
-        i += way * (40 * abs(nowY) + 5)
+        speed = 80 * abs(nowY)
+        if speed < 200:
+            speed = 200
+        i += way * speed
         if i < -10000:
             way = 1
             i += -1 * (i + 10000)
